4dcd7d763340d5ab49d0911217bd2cc7074fcd83
guiParoti <guilhermesouza67333@gmail.com>
2026-05-24 19:49:50 -0300

    gambiarra: agora a rota /meusfilmes retornam os filmes que batem com o id do filme referente a id_usuario em reviews

--- src/routes/filmes.ts
+++ src/routes/filmes.ts
@@ -15,7 +15,9 @@ router.post("/salvarfilme", async (req, res) => {
 
 
 router.get("/meusfilmes", async (req, res) => {
-    const [rows] = await database.execute<RowDataPacket[]>("SELECT * FROM filmes")
+    const { id_user } = req.query  // req.query pega parâmetros da URL — ex: /meusfilmes?id_user=1
+    // Infeliz quando criei a tabela de filmes no banco, não botei uma fk de usuários e tive que meter essa gambiarra de pegar os filmes que batem com o id do filme na tabela de reviews(que possui um fk_id_usuario)
+    const [rows] = await database.execute<RowDataPacket[]>(`SELECT * FROM filmes WHERE id_filme IN (SELECT id_filme FROM reviews WHERE id_usuario = ?)`, [Number(id_user)])
     res.json(rows)
 })
 
