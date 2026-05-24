2a26a0cc0d1f09a9f547c56b0465cf8b607c7280
guiParoti <guilhermesouza67333@gmail.com>
2026-05-24 19:46:26 -0300

    feats: agora meusfilmes retornam os filmes salvos no banco, não uso mais o FilmeContext

--- src/pages/MeusFilmes.tsx
+++ src/pages/MeusFilmes.tsx
@@ -1,7 +1,29 @@
-import { useFilme } from "../contexts/FilmeContext/UseFilme"
+import { useEffect, useState } from "react"
+import { buscarMeusFilmes, type FilmeSalvo } from "../services/filmes"
+import { useUsuario } from "../contexts/UsuarioContext/UseUsuario"
 
 export const MeusFilmes = () => {
-    const { filmes } = useFilme()
+    const { usuario } = useUsuario()
+    const [filmes, setFilmes] = useState<FilmeSalvo[] | null>([])
+    const [erro, setErro] = useState<string>('')
+    const [loading, setLoading] = useState<boolean>(false)
+
+
+    useEffect(() => {
+        const buscar = async () => {
+            setLoading(true)
+            try {
+                const dados = await buscarMeusFilmes(Number(usuario?.id_user))
+                setFilmes(dados as [])
+            } catch {
+                setErro('Nenhum filme salvo ainda!')
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+        buscar()
+    }, [])
 
     return (
         // padding geral da página
@@ -10,26 +32,30 @@ export const MeusFilmes = () => {
             <h3 className="text-3xl font-bold text-gray-800 mb-6">🎬 Meus Filmes</h3>
 
             {/* mensagem quando lista vazia */}
-            {filmes.length === 0 && (
-                <p className="text-gray-400 text-center mt-20">Nenhum filme salvo ainda!</p>
+            {erro && (
+                <p className="text-gray-400 text-center mt-20">{erro}</p>
+            )}
+    
+            {loading && (
+                <p className="text-gray-400 text-center mt-20">{loading}</p>
             )}
 
             {/* grid de 4 colunas — gap é o espaço entre os cards */}
             <div className="grid grid-cols-4 gap-6">
-                {filmes.map((filme) => (
+                {filmes?.map((filme) => (
                     
-                    <div key={filme.id}
+                    <div key={filme.id_filme}
                         className="bg-white rounded-xl shadow-md overflow-hidden cursor-pointer hover:shadow-xl transition-shadow">
                         
                         {/* w-full faz a imagem ocupar toda a largura do card */}
-                        <img src={`https://image.tmdb.org/t/p/w300${filme.poster_path}`} className="w-full"/>
+                        <img src={`https://image.tmdb.org/t/p/w300${filme.poster_url}`} className="w-full"/>
                         
                         {/* padding interno do card */}
                         <div className="p-3 flex flex-col gap-1">
-                            <span className="font-bold text-sm text-gray-700">{filme.title}</span>
-                            <span className="font-bold text-sm text-gray-700">{filme.release_date.slice(0, 4)}</span>
+                            <span className="font-bold text-sm text-gray-700">{filme.titulo}</span>
+                            {/*<span className="font-bold text-sm text-gray-700">{filme.release.slice(0, 4)}</span>/*}
                             {/* line-clamp-2 limita o texto a 2 linhas com ... no final */}
-                            <span className="text-xs text-gray-400 line-clamp-3">{filme.overview}</span>
+                            {/*<span className="text-xs text-gray-400 line-clamp-3">{filme.}</span>*/}
                         </div>
                     </div>
                 ))}
--- src/services/filmes.ts
+++ src/services/filmes.ts
@@ -1,5 +1,14 @@
 import type { Filme } from "./tmdb"
 
+// Dica para mim mesmo no futuro, padronize os nomes, homem!
+// Tive que criar uma interface propria dos filmes que vem do banco porque criei a tabela com a coluna "poter_url" e na tipagem do front eu uso poster_path
+export interface FilmeSalvo {
+    id_filme: number
+    tmdb_id: number
+    titulo: string
+    poster_url: string
+}
+
 export const salvarFilme = async (tmdb_id: number, titulo: string, poster_url: string) : Promise<Filme | null> => {
     try {
         const resposta = await fetch(`http://localhost:3333/filmes/salvarfilme`, {
@@ -11,6 +20,16 @@ export const salvarFilme = async (tmdb_id: number, titulo: string, poster_url: s
         return dados
     } catch  {
         return null
+    }
+}
+
+export const buscarMeusFilmes = async (id_user: number) : Promise<Filme[] | null> => {
+    try {
+        const resposta = await fetch(`http://localhost:3333/filmes/meusfilmes?id_user=${id_user}`)
+        const dados = await resposta.json()
+        return dados
+    } catch {
+        return []
     }
 }
 
