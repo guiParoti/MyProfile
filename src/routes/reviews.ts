40b57fe49c0cc2073674a2bed71de88ebfd94a8a
guiParoti <guilhermesouza67333@gmail.com>
2026-05-26 12:53:01 -0300

    feats: /remover para excluir um filme e uma review, apaga das duas tabelas de uma vez

--- src/routes/reviews.ts
+++ src/routes/reviews.ts
@@ -17,8 +17,15 @@ router.post("/novareview", async (req, res) => {
 })
 
 router.get("/minhasreviews", async (req, res) => {
-    const [rows] = await database.execute<RowDataPacket[]>("SELECT * FROM reviews")
-    res.json({rows})
+    const { id_usuario, id_filme } = req.query
+    const [rows] = await database.execute<RowDataPacket[]>(`SELECT * FROM reviews WHERE id_usuario = ? AND id_filme = ?`, [Number(id_usuario), Number(id_filme)])
+    res.json(rows)
 })
 
+router.delete("/minhasreviews/remover", async (req, res) => {
+    const { id_review, id_filme } = req.body
+    await database.execute("DELETE FROM reviews WHERE id_review = ?", [id_review])
+    await database.execute("DELETE FROM filmes WHERE id_filme = ?", [id_filme])
+    res.json({mensagem: "Filme removido!"})
+})
 export default router
